95a8a6316c19836c6450cd12490d0af7b5fa43b9
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-03-28 13:35:59 +0200

    add friends routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,12 @@
-const USERS = require('../models/User');
-const express = require('express');
+const express = require("express");
+const { sendFriendRequest, acceptFriendRequest, removeFriend,getFriends } = require("../controllers/userController");
+const auth = require("../middleware/authorize");
+
 const router = express.Router();
 
-router.get('/',async(req,res) => {
-    try {
-        const users = await USERS.find();
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({ error: "Server error" });
-    }
-})
+router.post("/friend-request/:id", auth, sendFriendRequest);
+router.post("/accept-request/:id", auth, acceptFriendRequest);
+router.delete("/remove-friend/:id", auth, removeFriend);
+router.get("/friends",auth,getFriends)
 
 module.exports = router;
