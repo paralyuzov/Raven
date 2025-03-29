049206c6f3374e02536ebefde9d59b7f74f53ee6
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-03-29 15:51:17 +0200

    add getUserById route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { sendFriendRequest, acceptFriendRequest, removeFriend,getFriends } = require("../controllers/userController");
+const { sendFriendRequest, acceptFriendRequest, removeFriend,getFriends, getUserById } = require("../controllers/userController");
 const auth = require("../middleware/authorize");
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/friend-request/:id", auth, sendFriendRequest);
 router.post("/accept-request/:id", auth, acceptFriendRequest);
 router.delete("/remove-friend/:id", auth, removeFriend);
 router.get("/friends",auth,getFriends)
+router.get("/user/:id",getUserById)
 
 module.exports = router;
