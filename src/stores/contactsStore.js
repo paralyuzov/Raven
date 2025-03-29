f5ddeb95b04b83269194665ca23b21153283934a
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-03-29 15:54:12 +0200

    Update contactsStore.js

--- src/stores/contactsStore.js
+++ src/stores/contactsStore.js
@@ -1,5 +1,6 @@
 import axios from "../services/api";
 import { defineStore } from "pinia";
+import { useAuthStore } from "./authStore";
 
 export const useContactsStore = defineStore("contactsStore",{
     state : () => ({
@@ -18,6 +19,33 @@ export const useContactsStore = defineStore("contactsStore",{
             } catch (error) {
                 console.log(error);
             }
-        }
+        },
+        async acceptFriendRequest(friendId) {
+            this.loading = true;
+            this.error = null;
+            try {
+                console.log(friendId)
+                await axios.post(`users/accept-request/${friendId}`);
+
+                const authStore = useAuthStore();
+                authStore.user.friendRequests = authStore.user.friendRequests.filter(id => id !== friendId);
+
+                await this.getContacts();
+            } catch (error) {
+                this.error = error.response?.data?.message || "Failed to accept friend request.";
+                console.error(error);
+            } finally {
+                this.loading = false;
+            }
+        },
+        async fetchUsernameById(userId) {
+            try {
+              const response = await axios.get(`users/user/${userId}`);
+              return response.data;
+            } catch (error) {
+              console.log(error);
+              return null;
+            }
+          },
     }
 })
