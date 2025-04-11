5590fb30a0eef2218485ffcc95e228dbc8f3bafb
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-04-11 14:33:07 +0300

    add seen emit functionality to the server

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const Message = require("./models/Message");
 dotenv.config();
 
 const app = express();
-const server = http.createServer(app); //
+const server = http.createServer(app);
 const io = socketIo(server, { 
   cors: {
     origin: "http://localhost:5173",
@@ -36,37 +36,65 @@ io.on("connection", (socket) => {
     console.log(`${userId} has joined with socket id: ${socket.id}`);
 
     try {
-      const unreadMessages = await Message.find({ recipient: userId, seen: false });
-
-      if (unreadMessages.length > 0) {
-        io.to(socket.id).emit("unread_messages", unreadMessages);
-
-        await Message.updateMany({ recipient: userId, seen: false }, { seen: true });
-      }
+        const unreadMessages = await Message.find({ recipient: userId, seen: false });
+        if (unreadMessages.length > 0) {
+            io.to(socket.id).emit("unread_messages", unreadMessages);
+        }
     } catch (err) {
-      console.error("Error fetching unread messages:", err);
+        console.error("Error fetching unread messages:", err);
     }
-  });
+});
 
   socket.on("private_message", async (message) => {
     const recipientSocket = users[message.recipient];
 
     try {
-
       const newMessage = new Message({
         sender: message.sender,
         recipient: message.recipient,
         message: message.message,
+        type: message.type || 'text',
         seen: false,
       });
       await newMessage.save();
 
       if (recipientSocket) {
-        io.to(recipientSocket).emit("receive_message", message);
-        await Message.findByIdAndUpdate(newMessage._id, { seen: true });
+        io.to(recipientSocket).emit("receive_message", {
+          ...message,
+          _id: newMessage._id,
+          seen: false
+        });
+
+        const unreadMessages = await Message.find({ 
+          recipient: message.recipient, 
+          sender: message.sender,
+          seen: false 
+        });
+        
+        io.to(recipientSocket).emit("unread_messages", unreadMessages);
       }
     } catch (err) {
       console.error("Error saving message:", err);
+    }
+  });
+
+  socket.on("mark_as_seen", async (data) => {
+    const { senderId, recipientId, socketId } = data;
+
+    try {
+      await Message.updateMany(
+        { sender: senderId, recipient: recipientId, seen: false },
+        { $set: { seen: true } }
+      );
+      
+      io.to(socketId).emit("messages_marked_as_seen", { senderId });
+      
+      const senderSocket = users[senderId];
+      if (senderSocket) {
+        io.to(senderSocket).emit("messages_marked_as_seen", { senderId: recipientId });
+      }
+    } catch (err) {
+      console.error("Error marking messages as seen:", err);
     }
   });
 
