394121d602adc435c534e412af5ea0422b70adef
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-04-27 15:16:31 +0300

    add path to profile page

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import RegistrationPage from "../views/RegistrationPage.vue";
 import LoginPage from "../views/LoginPage.vue";
 import ChatPage from "../views/ChatPage.vue";
+import ProfilePage from "../views/ProfilePage.vue";
 
 const routes = [
   { path: "/", component: RegistrationPage },
   { path: "/login", component: LoginPage },
   { path: "/messages",name:"messages", component: ChatPage },
+  {path: "/profile",name:"profile", component:ProfilePage},
 ];
 
 const router = createRouter({
