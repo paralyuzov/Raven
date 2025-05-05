ee8736eed4408213e08cd5f0de4ce56d3f034594
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-05-05 23:58:18 +0300

    create route to fetch current user data

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/User");
+const authorize = require("../middleware/authorize");
 
 const router = express.Router();
 
@@ -53,6 +54,7 @@ router.post("/login", async (req, res) => {
     }
 
     res.cookie("user", user._id.toString(), COOKIE_OPTIONS);
+    console.log(user)
     res.status(200).json({
       user: {
         id: user._id,
@@ -76,6 +78,31 @@ router.post("/logout", (req, res) => {
   res.status(200).json({ message: "Logout successful" });
 });
 
-
+router.get("/me", authorize, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+    
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    
+    res.status(200).json({
+      user: {
+        id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        nickname: user.nickname,
+        email: user.email,
+        avatar: user.avatar,
+        friendRequests: user.friendRequests,
+        friends: user.friends
+      }
+    });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 module.exports = router;
