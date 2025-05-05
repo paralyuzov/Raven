bc8d72f50ed98aff29d350824487e1486751c6bf
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-05-05 23:57:50 +0300

    change app background

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import NavigationBottom from './components/NavigationBottom.vue';
 </script>
 
 <template>
-    <div class="flex flex-col min-h-screen bg-slate-700">
+    <div class="flex flex-col min-h-screen bg-slate-900">
       <div class="">
         <router-view />
       </div>
