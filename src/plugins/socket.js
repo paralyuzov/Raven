3c77dec6b80ad4668e5113d5ee2649943c7330e2
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-05-07 00:46:21 +0300

    Update socket.js

--- src/plugins/socket.js
+++ src/plugins/socket.js
@@ -1,15 +1,20 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_URL || "http://localhost:3002",{
-    withCredentials: true,
-    transports: ["websocket"],
-    autoConnect: false,
-    reconnectionAttempts: 5,
-    reconnectionDelay: 1000,
-    timeout: 20000,
-    upgrade: false,
-    pingTimeout: 20000,
-    pingInterval: 25000
-}); 
+const socket = io(import.meta.env.VITE_API_URL || "http://localhost:3002", {
+  withCredentials: true,
+  transports: ["websocket"],
+  autoConnect: false,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+  timeout: 20000,
+  upgrade: false,
+  pingTimeout: 20000,
+  pingInterval: 25000,
+  auth: (cb) => {
+    cb({
+      token: localStorage.getItem('auth_token')
+    });
+  }
+});
 
 export default socket;
