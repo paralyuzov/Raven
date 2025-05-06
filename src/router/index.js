0a5c6e2025c0ccf2b330fce06b06871edec8f7d0
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-05-06 21:03:42 +0300

    add guest and auth guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,71 @@
-import { createRouter, createWebHistory } from "vue-router";
-import RegistrationPage from "../views/RegistrationPage.vue";
-import LoginPage from "../views/LoginPage.vue";
-import ChatPage from "../views/ChatPage.vue";
-import ProfilePage from "../views/ProfilePage.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '../stores/authStore'
+
+import AuthLayout from '../layouts/AuthLayout.vue'
+import GuestLayout from '../layouts/GuestLayout.vue'
+
+import LoginPage from '../views/LoginPage.vue'
+import RegistrationPage from '../views/RegistrationPage.vue'
+
+import ChatPage from '../views/ChatPage.vue'
+import ProfilePage from '../views/ProfilePage.vue'
+
 
 const routes = [
-  { path: "/", component: RegistrationPage },
-  { path: "/login", component: LoginPage },
-  { path: "/messages",name:"messages", component: ChatPage },
-  {path: "/profile",name:"profile", component:ProfilePage},
-];
+  {
+    path: '/',
+    component: GuestLayout,
+    children: [
+      {
+        path: '',
+        name: 'registration',
+        component: RegistrationPage
+      },
+      {
+        path: 'login',
+        name: 'login',
+        component: LoginPage
+      }
+    ],
+    meta: { requiresGuest: true }
+  },
+  {
+    path: '/',
+    component: AuthLayout,
+    children: [
+      {
+        path: 'messages',
+        name: 'messages',
+        component: ChatPage
+      },
+      {
+        path: 'profile',
+        name: 'profile',
+        component: ProfilePage
+      },
+    ],
+    meta: { requiresAuth: true }
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
-});
+  routes
+})
 
-export default router;
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  const isLoggedIn = authStore.isLoggedIn
+  
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    return next('/login')
+  }
+  
+  if (to.meta.requiresGuest && isLoggedIn) {
+    return next('/messages')
+  }
+  
+  return next()
+})
+
+export default router
