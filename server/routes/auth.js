95072fdefefbcdecb7cf46bb2eff248bc02fe29c
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-05-06 21:59:05 +0300

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,10 +6,9 @@ const router = express.Router();
 
 const COOKIE_OPTIONS = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", 
+  secure: true,
+  sameSite: 'None',
   maxAge: 24 * 60 * 60 * 1000,
-  domain: process.env.NODE_ENV === "production" ? process.env.COOKIE_DOMAIN : undefined
 };
 
 router.post("/register", async (req, res) => {
