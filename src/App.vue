d0613a4642a970f07ea2580f599f56fcd7f961a7
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-05-06 21:04:27 +0300

    update app layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <script setup>
-import NavigationBottom from './components/NavigationBottom.vue';
 
 </script>
 
 <template>
-    <div class="flex flex-col min-h-screen bg-slate-900">
-      <div class="">
-        <router-view />
-      </div>
-      <NavigationBottom />
-    </div>
-  </template>
+  <router-view />
+</template>
